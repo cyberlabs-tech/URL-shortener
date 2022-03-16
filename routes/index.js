51a13f38128220a286dfe7e0b58f984ecda4b610
Manh Tran <tienmanh.tran68@gmail.com>
2022-03-16 15:36:18 +0700

    Create the GET method to restore to the original URL

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,18 @@ const router = express.Router();
 // @desc        Redirect to the long / original URL
 router.get("/:code", async (req, res) => {
   try {
-    const client = require("../config/db");
+    const { client } = require("../config/db");
     const longUrl = await client.get(req.params.code);
 
     if (longUrl !== null) {
       return res.redirect(longUrl);
+    } else {
+      return res.status(404).json("No url found");
     }
-  } catch (err) {}
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Server error");
+  }
 });
 
 module.exports = router;
