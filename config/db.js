d546a00db9daffc268d838826eb10770d7e1c03c
Manh Tran <tienmanh.tran68@gmail.com>
2022-03-16 12:48:04 +0700

    Connecting Redis worked!

--- config/db.js
+++ config/db.js
@@ -3,15 +3,19 @@ const config = require("config");
 
 const db = config.get("redisConfig");
 
-const connectDB = () => {
+const connectDB = async () => {
   const client = redis.createClient({
     host: db.host,
     port: db.port,
   });
 
-  client.on("error", (err) => {
-    console.log("Error :" + err);
-  });
+  client.on("error", (err) => console.log("Redis Client Error", err));
 
-  console.log("Redis connected...");
+  await client.connect();
+
+  await client.set("Mike", "Tran");
+  const value = await client.get("Mike");
+  console.log(value);
 };
+
+connectDB();
